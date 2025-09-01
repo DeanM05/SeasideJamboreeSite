f8c27692be7ac319b075d2ee0b382eb5eed50a02
Dean M <deanmalo123@gmail.com>
2025-09-01 15:15:10 +0100

    Init

---
+++ src/_data/meta.js
@@ -0,0 +1,8 @@
+module.exports = {
+  url: process.env.URL || "http://localhost:8080",
+  siteName: "TeaHertz Studio",
+  siteSubName: "Audio Mixing & Mastering",
+  siteDescription:
+    "Crafting Emotionally Competitive Mixes",
+  authorName: "Dean AM",
+};
